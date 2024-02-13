b971d539cee34e84c796762be644480d49817910
Tzepes <andreicristian.vlad@gmail.com>
2024-02-13 23:11:06 +0200

    goerli network update

--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,10 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonicPhrase = fs.readFileSync(".secret").toString().trim();
+const infuraProjectID = fs.readFileSync(".infura").toString().trim();
 
 module.exports = {
   /**
@@ -56,6 +59,11 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: fs.readFileSync('.etherscan').toString().trim()
+  },
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -87,13 +95,13 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonicPhrase, `https://goerli.infura.io/v3/${infuraProjectID}`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
